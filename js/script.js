e140bd2ff9fa56613f533f887d2841fef37534cb
Ava <99355031+AvaVenturino@users.noreply.github.com>
2022-03-21 17:28:52 +0000

    added code

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2022 Ava Venturino All rights reserved
+//
+// Created by: Ava Venturino
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+/**
+ * This function gets users address and shows it back to user.
+ */
+
+function enterClicked() {
+  // input
+  const streetNumber = document.getElementById("street-number").value
+  const streetName = parseInt(document.getElementById("street-name").value)
